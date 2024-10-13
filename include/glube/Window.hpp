c865544b2209d6390a6cb3117f3e5aca5022b6ef
Mumbly Juergens <mumblyjuergens@gmail.com>
2024-10-13 18:16:48 +1100

    refactor: cleaned Window (lol), fixed for dll export

--- include/glube/Window.hpp
+++ include/glube/Window.hpp
@@ -5,6 +5,7 @@
 #include <GLFW/glfw3.h>
 #include "glube/Context.hpp"
 #include "glube/keys.hpp"
+#include "glube/glube_export.h"
 
 namespace glube
 {
@@ -15,34 +16,52 @@ namespace glube
         KeyEventHandler *keyEventHandler{};
         void *userPointer{};
 
-        static void error_callback(int error, const char *description);
-        static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
+        GLUBE_EXPORT static void error_callback(int error, const char *description);
+        GLUBE_EXPORT static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
 
     public:
         [[nodiscard]] Window() noexcept = default;
         [[nodiscard]] Window(const int width, const int height, const std::string_view title) { init(width, height, title); }
-        [[nodiscard]] Window(Window &&other) noexcept;
-        Window &operator=(Window &&other) noexcept;
         Window(const Window &) = delete;
         Window &operator=(const Window &) = delete;
-        [[nodiscard]] bool try_init(const int width, const int height, const std::string_view title);
-        void init(const int width, const int height, const std::string_view title);
-        ~Window();
+        [[nodiscard]] Window(Window &&other) noexcept
+        {
+            window = std::exchange(other.window, nullptr);
+            keyEventHandler = std::exchange(other.keyEventHandler, nullptr);
+            userPointer = std::exchange(other.userPointer, nullptr);
+        }
+        Window &operator=(Window &&other) noexcept
+        {
+            window = std::exchange(other.window, nullptr);
+            keyEventHandler = std::exchange(other.keyEventHandler, nullptr);
+            userPointer = std::exchange(other.userPointer, nullptr);
+            return *this;
+        }
+        ~Window() { glfwDestroyWindow(window); }
+        [[nodiscard]] GLUBE_EXPORT bool try_init(const int width, const int height, const std::string_view title);
+        void init(const int width, const int height, const std::string_view title)
+        {
+            if (!try_init(width, height, title))
+            {
+                throw std::runtime_error{"Could not initialise window"};
+            }
+        }
 
         [[nodiscard]] GLFWwindow *get() const noexcept { return window; }
         [[nodiscard]] bool good() const noexcept { return window; }
 
-        void SwapInterval(const bool minimum) { glfwSwapInterval(minimum); }
+        void swap_interval(const bool minimum) { glfwSwapInterval(minimum); }
 
-        void SetKeyEventHandler(KeyEventHandler *handler) noexcept { keyEventHandler = handler; }
+        void set_key_event_handler(KeyEventHandler *handler) noexcept { keyEventHandler = handler; }
 
-        [[nodiscard]] bool ShouldClose() const { return glfwWindowShouldClose(window); }
-        void SetShouldClose(const bool value) { glfwSetWindowShouldClose(window, value); }
+        [[nodiscard]] bool should_close() const { return glfwWindowShouldClose(window); }
+        void set_should_close(const bool value) { glfwSetWindowShouldClose(window, value); }
 
-        void SwapBuffers() { glfwSwapBuffers(window); }
-        void PollEvents() { glfwPollEvents(); }
+        void swap_buffers() { glfwSwapBuffers(window); }
+        void poll_events() { glfwPollEvents(); }
 
-        [[nodiscard]] void *UserPointer() const noexcept { return userPointer; }
-        void SetUserPointer(void *value) noexcept { userPointer = value; }
+        [[nodiscard]] void *user_pointer() const noexcept { return userPointer; }
+        void set_user_pointer(void *value) noexcept { userPointer = value; }
     };
+
 } // End namespace glube.
--- src/Window.cpp
+++ src/Window.cpp
@@ -11,27 +11,12 @@ namespace glube
         void opengl_debug_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam);
     }
 
-    void Window::error_callback(int error, const char *description)
+    GLUBE_EXPORT void Window::error_callback(int error, const char *description)
     {
         std::println(stderr, "GLFW error {0}: {1}", error, description);
     }
 
-    Window::Window(Window &&other) noexcept
-    {
-        window = std::exchange(other.window, nullptr);
-        keyEventHandler = std::exchange(other.keyEventHandler, nullptr);
-        userPointer = std::exchange(other.userPointer, nullptr);
-    }
-
-    Window &Window::operator=(Window &&other) noexcept
-    {
-        window = std::exchange(other.window, nullptr);
-        keyEventHandler = std::exchange(other.keyEventHandler, nullptr);
-        userPointer = std::exchange(other.userPointer, nullptr);
-        return *this;
-    }
-
-    void Window::key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+    GLUBE_EXPORT void Window::key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
     {
         Window *me = reinterpret_cast<Window *>(glfwGetWindowUserPointer(window));
         if (me->keyEventHandler)
@@ -46,7 +31,7 @@ namespace glube
         }
     }
 
-    bool Window::try_init(const int width, const int height, const std::string_view title)
+    GLUBE_EXPORT bool Window::try_init(const int width, const int height, const std::string_view title)
     {
         glfwSetErrorCallback(error_callback);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -63,19 +48,6 @@ namespace glube
         glEnable(GL_DEBUG_OUTPUT);
         glDebugMessageCallback(internal::opengl_debug_callback, nullptr);
         return window;
-    }
-
-    void Window::init(const int width, const int height, const std::string_view title)
-    {
-        if (!try_init(width, height, title))
-        {
-            throw std::runtime_error{"Could not initialise window"};
-        }
-    }
-
-    Window::~Window()
-    {
-        glfwDestroyWindow(window);
     }
 
 } // End namespace glube.
